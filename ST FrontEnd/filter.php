de38eec2b14c865d30ba4b80d6bc2ff80a6eeea6
Pantawhite <nguyenphucminh880@gmail.com>
2024-03-07 11:24:15 +0700

    Beauty code

--- ST FrontEnd/filter.php
+++ ST FrontEnd/filter.php
@@ -70,96 +70,104 @@ $arrWine = array(
 
 
 <div class="product-tag product-list-container">
-<div class="attrlist-actions">
-<div class="container">
-<div class="faceted-search-container" data-select2-id="select2-data-27-po28">
-<div class="faceted-search" id="box_select_filter">
-    <form action="Sanpham.php" method="get">
-<div class="facet-group">
-    <label class="facet-label">KHOẢNG GIÁ</label>
-    <div id="facet-body-categoryid" class="facet-body">
-        <select name="price_space" id="input_filter_price_space"  class="select2" value="<?php if(isset($_GET['Gia'])) {echo $_GET['Gia'];   }?>">
-             <option class="list_filter_title toggleCtl change_btn_minus" value="">-- Tất cả --</option>
-            <option value="90000-300000">90,000 - 300,000</option>
-            <option value="300000-500000">300,000 - 500,000</option>
-            <option value="500000-700000">500,000 - 700,000</option>
-            <option value="700000-1000000">700,000 - 1,000,000</option>
-            <option value="1000000-5000000">1,000,000 - 5,000,000</option>
-            <option value="5000000-99999999"> > 5,000,000</option>
-        </select>
+    <div class="attrlist-actions">
+        <div class="container">
+            <div class="faceted-search-container" data-select2-id="select2-data-27-po28">
+                <div class="faceted-search" id="box_select_filter">
+                    <form action="Sanpham.php" method="get">
+                        <div class="facet-group">
+                            <label class="facet-label">KHOẢNG GIÁ</label>
+                            <div id="facet-body-categoryid" class="facet-body">
+                                <select name="price_space" id="input_filter_price_space" class="select2" value="<?php if (isset($_GET['Gia'])) {
+                                                                                                                    echo $_GET['Gia'];
+                                                                                                                } ?>">
+                                    <option class="list_filter_title toggleCtl change_btn_minus" value="">-- Tất cả --</option>
+                                    <option value="90000-300000">90,000 - 300,000</option>
+                                    <option value="300000-500000">300,000 - 500,000</option>
+                                    <option value="500000-700000">500,000 - 700,000</option>
+                                    <option value="700000-1000000">700,000 - 1,000,000</option>
+                                    <option value="1000000-5000000">1,000,000 - 5,000,000</option>
+                                    <option value="5000000-99999999"> > 5,000,000</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="facet-group">
+                            <label class="facet-label">THƯƠNG HIỆU</label>
+                            <div class="facet-body">
+                                <select name="attrs[]" class="select2" id="input_filter_brand" tabindex="0" aria-hidden="false" value="<?php if (isset($_GET["Idthuonghieu"])) {
+                                                                                                                                            echo $_GET["Idthuonghieu"];
+                                                                                                                                        } ?>">
+                                    <option class="list_filter_title toggleCtl change_btn_minus" value="">-- Tất cả --</option>
+                                    <option value="HE">Hennessy</option>
+                                    <option value="BW">Blended Whisky</option>
+                                    <option value="RU">Rum</option>
+                                    <option value="AB">Absolute</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="facet-group">
+                            <label class="facet-label">THỂ TÍCH</label>
+                            <div class="facet-body">
+                                <select name="attrs[]" class="select2" id="input_filter_volume" tabindex="0" aria-hidden="false" value="<?php if (isset($_GET["Dungtich"])) {
+                                                                                                                                            echo $_GET["Dungtich"];
+                                                                                                                                        } ?>">
+                                    <option class="list_filter_title toggleCtl change_btn_minus" value="">-- Tất cả --</option>
+                                    <option value="0-499">
+                                        < 500ml </option>
+                                    <option value="500-750"> 500ml - 750ml </option>
+                                    <option value="1000-9999"> > 1000ml </option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="facet-group">
+                            <label class="facet-label">NỒNG ĐỘ CỒN</label>
+                            <div class="facet-body">
+                                <select name="attrs[]" class="select2" id="input_filter_alcohol" tabindex="0" aria-hidden="false">
+                                    <option class="list_filter_title toggleCtl change_btn_minus" value="">-- Tất cả --</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="facet-group">
+                            <label class="facet-label">XUẤT XỨ</label>
+                            <div class="facet-body">
+                                <select name="attrs[]" class="select2" id="input_filter_" tabindex="0" aria-hidden="false">
+                                    <option class="list_filter_title toggleCtl change_btn_minus" value="">-- Tất cả --</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="facet-group">
+                            <label class="facet-label">LOẠI VANG</label>
+                            <div class="facet-body">
+                                <select name="attrs[]" class="select2" id="input_filter_" tabindex="0" aria-hidden="false">
+                                    <option class="list_filter_title toggleCtl change_btn_minus" value="">-- Tất cả --</option>
+                                </select>
+                                <div class="facet-group">
+                                    <input type="button" class="btn btn-outline-primary btn-filter" onclick="filterProducts()" value="Lọc">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" name="search1" value="<?php if (isset($_GET["serach1"])) {
+                                                                                                                                                                                    echo $_GET["serach1"];
+                                                                                                                                                                                } ?>">
+                            <button type="submit" class="btn btn-outline-primary" data-mdb-ripple-init>search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
-<div class="facet-group">
-<label class="facet-label">THƯƠNG HIỆU</label>
-<div class="facet-body">
-<select name="attrs[]" class="select2" id="input_filter_brand" tabindex="0" aria-hidden="false" value="<?php if(isset($_GET["Idthuonghieu"])) {echo $_GET["Idthuonghieu"];} ?>">
-<option class="list_filter_title toggleCtl change_btn_minus" value="">-- Tất cả --</option>
-<option value="HE">Hennessy</option>
-<option value="BW">Blended Whisky</option>
-<option value="RU">Rum</option>
-<option value="AB">Absolute</option>
-</select>
-</div>
-</div>
-<div class="facet-group">
-<label class="facet-label">THỂ TÍCH</label>
-<div class="facet-body">
-<select name="attrs[]" class="select2" id="input_filter_volume" tabindex="0" aria-hidden="false"  value="<?php if(isset($_GET["Dungtich"])) {echo $_GET["Dungtich"];} ?>">
-<option class="list_filter_title toggleCtl change_btn_minus" value="">-- Tất cả --</option>
-<option value="0-499"> < 500ml </option>
-<option value="500-750"> 500ml - 750ml </option>
-<option value="1000-9999"> > 1000ml </option>
-</select>
-</div>
-</div>
-<div class="facet-group">
-<label class="facet-label">NỒNG ĐỘ CỒN</label>
-<div class="facet-body">
-<select name="attrs[]" class="select2" id="input_filter_alcohol" tabindex="0" aria-hidden="false">
-<option class="list_filter_title toggleCtl change_btn_minus" value="">-- Tất cả --</option>
-</select>
-</div>
-</div>
-<div class="facet-group">
-<label class="facet-label">XUẤT XỨ</label>
-<div class="facet-body">
-<select name="attrs[]" class="select2" id="input_filter_" tabindex="0" aria-hidden="false">
-<option class="list_filter_title toggleCtl change_btn_minus" value="" >-- Tất cả --</option>
-</select>
-</div>
-</div>
-<div class="facet-group">
-<label class="facet-label">LOẠI VANG</label>
-<div class="facet-body">
-<select name="attrs[]" class="select2" id="input_filter_" tabindex="0" aria-hidden="false">
-<option class="list_filter_title toggleCtl change_btn_minus" value="" >-- Tất cả --</option>
-</select>
-<div class="facet-group">
-<input type="button" class="btn btn-outline-primary btn-filter" onclick="filterProducts()" value="Lọc">
-
-</div>
-</div>
-</div>
-<div class="input-group"> 
-  <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" name="search1" 
-  value="<?php if(isset($_GET["serach1"])) {echo $_GET["serach1"];} ?>">
-  <button type="submit" class="btn btn-outline-primary" data-mdb-ripple-init>search</button>
-</form>
-</div>
-</div>
-</div>
-</div>
-</div>
 </div>
 </div>
 
 
 
-  <script>
-   function filterProducts() {
+<script>
+    function filterProducts() {
         var priceSpaceSelect = document.getElementById('input_filter_price_space');
         var priceSpace = priceSpaceSelect.value;
-        
+
         var brandSelect = document.getElementById('input_filter_brand');
         var brand = brandSelect.value;
 
@@ -170,15 +178,15 @@ $arrWine = array(
         var search1 = searchInput.value;
 
         // Chuyển hướng trang với các tham số được chọn
-        window.location.href = 'Sanpham.php?search1=' + encodeURIComponent(search1) + '&price_space=' + priceSpace + '&brand=' + brand + '&volume=' + volume;}
+        window.location.href = 'Sanpham.php?search1=' + encodeURIComponent(search1) + '&price_space=' + priceSpace + '&brand=' + brand + '&volume=' + volume;
+    }
 
 
 
 
-         // Kiểm tra xem trình duyệt có hỗ trợ History API hay không
+    // Kiểm tra xem trình duyệt có hỗ trợ History API hay không
     if (typeof window.history.replaceState === 'function') {
         // Xóa tham số truy vấn từ URL và thay đổi URL
         window.history.replaceState({}, document.title, window.location.pathname);
     }
-
 </script>
