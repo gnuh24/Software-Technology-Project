f57e21844070ed11963283f0f52c16ee383ee4a4
VincentTuannn <147407930+VincentTuannn@users.noreply.github.com>
2024-05-01 23:29:21 +0700

    Thêm vào chức năng update cho loại sản phẩm

--- Sources/FrontEnd/ManagerUI/QLLoaiSanPham/QLLoaiSanPham.php
+++ Sources/FrontEnd/ManagerUI/QLLoaiSanPham/QLLoaiSanPham.php
@@ -17,7 +17,7 @@
         <div class="StaffLayout_wrapper__CegPk">
           <div class="StaffHeader_wrapper__IQw-U">
             <p class="StaffHeader_title__QxjW4">Dekanta</p>
-            <button class="StaffHeader_signOut__i2pcu">
+            <button id="logoutButton" class="StaffHeader_signOut__i2pcu">
               <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right-from-bracket" class="svg-inline--fa fa-arrow-right-from-bracket" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width: 2rem; height: 2rem; color: white">
                 <path fill="currentColor" d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"></path>
               </svg>
@@ -26,29 +26,29 @@
           <div>
             <div>
               <div class="Manager_wrapper__vOYy">
-              <div class="Sidebar_sideBar__CC4MK">
-                    <a class="MenuItemSidebar_menuItem__56b1m" href="../QLLoaiSanPham/QLLoaiSanPham.php">
-                        <span class="MenuItemSidebar_title__LLBtx">Loại Sản Phẩm</span>
-                    </a>
-                    <a class="MenuItemSidebar_menuItem__56b1m" href="../QLSanPham/QLSanPham.php">
-                        <span class="MenuItemSidebar_title__LLBtx">Sản Phẩm</span>
-                    </a>
-                    <a class="MenuItemSidebar_menuItem__56b1m" href="../QLNhaCungCap/QLNhaCungCap.php">
-                        <span class="MenuItemSidebar_title__LLBtx">Nhà Cung Cấp</span>
-                    </a>
-                    <a class="MenuItemSidebar_menuItem__56b1m" href="../QLPhieuNhapKho/QLPhieuNhapKho.php">
-                        <span class="MenuItemSidebar_title__LLBtx">Phiếu Nhập Kho</span>
-                    </a>
-                    <a class="MenuItemSidebar_menuItem__56b1m" href="../QLDonHang/QLDonHang.php">
-                        <span class="MenuItemSidebar_title__LLBtx">Đơn Hàng</span>
-                    </a>
-                    <a class="MenuItemSidebar_menuItem__56b1m" href="../ThongKe/ThongKeDoanhThuChiTieu.php">
-                        <span class="MenuItemSidebar_title__LLBtx">Thống Kê Tài Chính</span>
-                    </a>                         
-                    </a>
-                    <a class="MenuItemSidebar_menuItem__56b1m" href="../ThongKe/ThongKeDonHang.php">
-                        <span class="MenuItemSidebar_title__LLBtx">Thống Kê Đơn Hàng</span>
-                    </a>
+                <div class="Sidebar_sideBar__CC4MK">
+                  <a class="MenuItemSidebar_menuItem__56b1m" href="../QLLoaiSanPham/QLLoaiSanPham.php">
+                    <span class="MenuItemSidebar_title__LLBtx">Loại Sản Phẩm</span>
+                  </a>
+                  <a class="MenuItemSidebar_menuItem__56b1m" href="../QLSanPham/QLSanPham.php">
+                    <span class="MenuItemSidebar_title__LLBtx">Sản Phẩm</span>
+                  </a>
+                  <a class="MenuItemSidebar_menuItem__56b1m" href="../QLNhaCungCap/QLNhaCungCap.php">
+                    <span class="MenuItemSidebar_title__LLBtx">Nhà Cung Cấp</span>
+                  </a>
+                  <a class="MenuItemSidebar_menuItem__56b1m" href="../QLPhieuNhapKho/QLPhieuNhapKho.php">
+                    <span class="MenuItemSidebar_title__LLBtx">Phiếu Nhập Kho</span>
+                  </a>
+                  <a class="MenuItemSidebar_menuItem__56b1m" href="../QLDonHang/QLDonHang.php">
+                    <span class="MenuItemSidebar_title__LLBtx">Đơn Hàng</span>
+                  </a>
+                  <a class="MenuItemSidebar_menuItem__56b1m" href="../ThongKe/ThongKeDoanhThuChiTieu.php">
+                    <span class="MenuItemSidebar_title__LLBtx">Thống Kê Tài Chính</span>
+                  </a>
+                  </a>
+                  <a class="MenuItemSidebar_menuItem__56b1m" href="../ThongKe/ThongKeDonHang.php">
+                    <span class="MenuItemSidebar_title__LLBtx">Thống Kê Đơn Hàng</span>
+                  </a>
                 </div>
                 <div style="padding-left: 16%; width: 100%; padding-right: 2rem">
                   <div class="wrapper">
@@ -75,8 +75,9 @@
                     <br>
                     <div class="boxFeature">
                       <div style="position: relative">
-                      <i class="fa fa-search"></i>
-                      <input class="input" placeholder="Tìm kiếm loại sản phẩm"/>
+                        <i class="fa fa-search"></i>
+                        <input class="Admin_input__LtEE-" placeholder="Tìm kiếm loại sản phẩm" />
+                        <button id="searchButton" style="cursor: pointer;"><i class="fa fa-search"></i></button>
                       </div>
 
 
@@ -93,24 +94,16 @@
                           </tr>
                         </thead>
                         <tbody id="tableBody">
-                        <?php
-                          // require_once "../../../BackEnd/ManagerBE/LoaiSanPhamBE.php";
 
-                          // $result = getAllLoaiSanPham(1, "");
-                          // // $totalPage = $result->totalPages;
-                          // $Ketqua = $result->data;
-
-                          // foreach ($Ketqua as $record) {
-                          //   echo '
-                          //         <tr>
-                          //           <td style="text-align:center">' . $record['maLoaiSanPham'] . '</td>
-                          //           <td style="text-align:center">' . $record['TenLoaiSanPham'] . '</td>
-                          //           <td style="text-align:center"><button>Xoá</button></td>
-                          //         </tr>';
-                          // }
-                          ?>
                         </tbody>
                       </table>
+                    </div>
+                    <div class="pagination">
+                      <?php
+                      for ($i = 1; $i <= $totalPage; $i++) {
+                        echo '<button class="pageButton" onclick="fetchDataAndUpdateTable(' . $i . ')">' . $i . '</button>';
+                      }
+                      ?>
                     </div>
                   </div>
                 </div>
@@ -122,16 +115,28 @@
     </div>
   </div>
 </body>
+
 </html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
+  // Lắng nghe sự kiện click trên nút logout
+  document.addEventListener('DOMContentLoaded', function() {
+    var logoutButton = document.getElementById('logoutButton');
+    if (logoutButton) {
+      logoutButton.addEventListener('click', function() {
+        localStorage.removeItem('key');
+        window.location.href = '../../../FrontEnd/MemberUI/Login/LoginUI.php';
+      });
+    }
+  });
+
   // Hàm để xóa hết các dòng trong bảng
   function clearTable() {
     var tableBody = document.querySelector('.Table_table__BWPy tbody');
     tableBody.innerHTML = ''; // Xóa nội dung trong tbody
   }
 
-  // Hàm getAllNhaCungCap
+  // Hàm getAllLoaiSanPham
   function getAllLoaiSanPham(page, search) {
     $.ajax({
       url: '../../../BackEnd/ManagerBE/LoaiSanPhamBE.php',
@@ -149,11 +154,22 @@
         // Duyệt qua mảng dữ liệu và tạo các hàng mới cho tbody
         data.forEach(function(record) {
           var trContent = `
-                        <tr>
-                            <td style="text-align:center">${record.MaLoaiSanPham}</td>
-                            <td style="text-align:center">${record.TenLoaiSanPham}</td>
-                            <td style="text-align:center"><button style="cursor:pointer" onclick="deleteLoaiSanPham(${record.MaLoaiSanPham})"><i class="fa-solid fa-trash"></i></button></td>
-                        </tr>`;
+         <form id="updateForm" method="POST" action="FormUpdateLoaiSanPham.php?MaLoaiSanPham=${record.MaLoaiSanPham}&TenLoaiSanPham=${record.TenLoaiSanPham}">
+    <tr>
+      <td style="text-align:center">${record.MaLoaiSanPham}</td>
+      <td style="text-align:center">${record.TenLoaiSanPham}</td>
+      <td style="text-align:center">`;
+
+          //Điều kiện để thêm nút xoá vào
+          if (record.MaLoaiSanPham != 1) {
+            trContent += `
+    <button style="cursor:pointer" class="edit" onclick="updateLoaiSanPham(${record.MaLoaiSanPham}, '${record.TenLoaiSanPham}')">Sửa</button>
+    |
+    <button style="cursor:pointer" class="delete" onclick="deleteLoaiSanPham(${record.MaLoaiSanPham})">Xoá</button>`;
+          }
+
+          trContent += `</td>
+    </tr>`;
 
           tableContent += trContent; // Thêm nội dung của hàng vào chuỗi tableContent
         });
@@ -171,7 +187,7 @@
     });
   }
 
-  // Hàm để gọi getAllLoaiSanPham và cập nhật dữ liệu và phân trang
+  // Hàm để gọi getAllNhaCungCap và cập nhật dữ liệu và phân trang
   function fetchDataAndUpdateTable(page, search) {
     //Clear dữ liệu cũ
     clearTable();
@@ -195,26 +211,33 @@
     // Xóa nút phân trang cũ (nếu có)
     paginationContainer.innerHTML = '';
 
-    // Tạo nút cho từng trang và thêm vào chuỗi HTML
-    var paginationHTML = '';
-    for (var i = 1; i <= totalPages; i++) {
-      paginationHTML += '<button class="pageButton">' + i + '</button>';
-    }
+    // Kiểm tra số lượng trang
+    if (totalPages > 1) {
+      // Tạo nút cho từng trang và thêm vào chuỗi HTML
+      var paginationHTML = '';
+      for (var i = 1; i <= totalPages; i++) {
+        paginationHTML += '<button class="pageButton">' + i + '</button>';
+      }
 
-    // Thiết lập nút phân trang vào paginationContainer
-    paginationContainer.innerHTML = paginationHTML;
+      // Thiết lập nút phân trang vào paginationContainer
+      paginationContainer.innerHTML = paginationHTML;
 
-    // Thêm sự kiện click cho từng nút phân trang
-    paginationContainer.querySelectorAll('.pageButton').forEach(function(button, index) {
-      button.addEventListener('click', function() {
-        // Gọi hàm fetchDataAndUpdateTable khi người dùng click vào nút phân trang
-        fetchDataAndUpdateTable(index + 1, searchValue); // Thêm 1 vào index để chuyển đổi về trang 1-indexed
+      // Thêm sự kiện click cho từng nút phân trang
+      paginationContainer.querySelectorAll('.pageButton').forEach(function(button, index) {
+        button.addEventListener('click', function() {
+          // Gọi hàm fetchDataAndUpdateTable khi người dùng click vào nút phân trang
+          fetchDataAndUpdateTable(index + 1, searchValue); // Thêm 1 vào index để chuyển đổi về trang 1-indexed
+        });
       });
-    });
 
-    // Đánh dấu trang hiện tại
-    paginationContainer.querySelector('.pageButton:nth-child(' + currentPage + ')').classList.add('active'); // Sửa lại để chỉ chọn trang hiện tại
+      // Đánh dấu trang hiện tại
+      paginationContainer.querySelector('.pageButton:nth-child(' + currentPage + ')').classList.add('active'); // Sửa lại để chỉ chọn trang hiện tại
+    } else {
+      // Khi chỉ có 1 trang, thêm class CSS để ẩn nút
+      paginationContainer.classList.add('single-page');
+    }
   }
+
 
 
   // Hàm xử lý sự kiện khi nút tìm kiếm được click
@@ -232,7 +255,7 @@
       // Ngăn chặn hành động mặc định của phím Enter (ví dụ: gửi form)
       event.preventDefault();
 
-      // Lấy giá trị của ô tìm kiếm
+      // Lấy giá trị của ô tìm kiếm và của select quyền
       var searchValue = this.value;
 
       // Truyền giá trị của biến currentPage vào hàm fetchDataAndUpdateTable
@@ -241,33 +264,43 @@
   });
 
   function deleteLoaiSanPham(MaLoaiSanPham) {
-  // Hiển thị hộp thoại xác nhận
-  var confirmation = confirm(`Bạn có muốn xóa loại sản phẩm ${MaLoaiSanPham} không?`);
+    var confirmation = confirm(`Bạn có muốn xóa nhà cung cấp ${MaLoaiSanPham} không?`);
 
-  // Kiểm tra xác nhận của người dùng
-  if (confirmation) {
-    // Gọi hàm deleteLoaiSanPham bằng Ajax
-    $.ajax({
-      url: '../../../BackEnd/ManagerBE/LoaiSanPhamBE.php',
-      type: 'POST',
-      dataType: "json",
-      data: {
-        MaNCC: MaNCC
-      },
-      success: function(response) {
-        // Nếu xóa thành công, reload bảng
-        if (response.status === 200) {
-          alert("Xóa loại sản phẩm thành công !!");
-          fetchDataAndUpdateTable(currentPage, '');
-        } else {
-          console.error('Lỗi khi xóa loại sản phẩm: ', response.message);
+    if (confirmation) {
+      $.ajax({
+        url: '../../../BackEnd/ManagerBE/LoaiSanPhamBE.php',
+        type: 'GET',
+        dataType: "json",
+        data: {
+          MaLoaiSanPham: MaLoaiSanPham
+        },
+        success: function(response) {
+          console.log('Status:', response.status); // In mã trạng thái
+          console.log('Message:', response.message); // In thông báo
+          console.log('MaLoaiSanPham:', MaLoaiSanPham);
+
+          if (response.status === 200) {
+            alert("Xóa nhà cung cấp thành công !!");
+            fetchDataAndUpdateTable(currentPage, '');
+          } else {
+            console.error('Lỗi khi xóa nhà cung cấp: ', response.message);
+          }
+        },
+        error: function(xhr, status, error) {
+          console.error('Lỗi khi gọi API: ', xhr, status, error);
         }
-      },
-      error: function(xhr, status, error) {
-        console.error('Lỗi khi gọi API: ', error);
-      }
-    });
+      });
+    }
   }
-}
 
+  function updateLoaiSanPham(MaLoaiSanPham, TenLoaiSanPham) {
+    // Lấy ra form bằng id của nó
+    var form = document.querySelector("#updateForm");
+
+    form.action = `FormUpdateLoaiSanPham.php?MaLoaiSanPham=${MaLoaiSanPham}&TenLoaiSanPham=${TenLoaiSanPham}`
+
+    // Gửi form đi
+    form.submit();
+
+  }
 </script>
